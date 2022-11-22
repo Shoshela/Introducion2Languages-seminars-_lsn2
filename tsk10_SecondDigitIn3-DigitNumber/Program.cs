f34cfb831584ec9fb6e134e661cff38fe35f284c
Shoshela <goyati@mail.ru>
2022-11-22 18:41:34 +0300

    Add tsk10_SecondDigitIn3-DigitNumber

---
+++ tsk10_SecondDigitIn3-DigitNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int number, secondDigit;
+Console.Write("Enter three-digit number... ");
+number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)  // проверка на трехзначность числа
+{                                   // производимые действия, если число трехзначное
+    secondDigit = (number - ((number/100)*100))/10; // определение второй цифре в числе
+    Console.Write("Second digit - ");
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Wrong number"); // сообщение в случае, если введенное число не трехзнначное
+}
