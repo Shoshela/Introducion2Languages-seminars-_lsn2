449c1897fdef7c39f587e4260e06617cbf14d14a
Shoshela <goyati@mail.ru>
2022-11-22 19:30:17 +0300

    Add tsk13_ThirdDigit

---
+++ tsk13_ThirdDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//  645 -> 5
+//  78 -> третьей цифры нет
+//  2679 -> 6
+
+Console.Write("Enter number... ");
+string stringNumber = Console.ReadLine();
+if (stringNumber.Length > 2)
+{
+  Console.WriteLine("Third digit - " + stringNumber[2]);
+}
+else 
+{
+  Console.WriteLine("... There is no third digit");
+}
